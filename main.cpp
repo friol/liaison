90d107835fd6aac3731e460bbd48f2d4b309d82d
friol <dantonag@gmail.com>
2023-12-12 07:15:41 +0100

    Working on it

--- main.cpp
+++ main.cpp
@@ -39,10 +39,10 @@ int main(int argc,char** argv)
 	}
 	else
 	{
-		//sourceFileName = "d:\\prova\\liaPrograms\\aoc12.2023.lia";
+		sourceFileName = "d:\\prova\\liaPrograms\\aoc12.2023.lia";
 		//sourceFileName = "d:\\prova\\liaPrograms\\aoc03.2015.lia"; // performance test
 		//sourceFileName = "d:\\prova\\liaPrograms\\test.lia";
-		sourceFileName = "d:\\prova\\liaPrograms\\ltest.lia";
+		//sourceFileName = "d:\\prova\\liaPrograms\\ltest.lia";
 	}
 
 	std::ifstream infile(sourceFileName);
